96c0efb881c3f663ccda6330944d5ccc783a323e
TwoSherlock <TwoSherlock@users.noreply.github.com>
2018-11-26 11:35:41 +0800

    添加登录页面动画插件

--- src/main/java/com/sm/config/SpringMVCConfig.java
+++ src/main/java/com/sm/config/SpringMVCConfig.java
@@ -1,10 +1,17 @@
-package com.sm.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.*;
-
-@Configuration
-public class SpringMVCConfig extends WebMvcConfigurationSupport {
-
-    //自定义mvc相关
-}
+//package com.sm.config;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.core.Ordered;
+//import org.springframework.web.servlet.config.annotation.*;
+//
+//@Configuration
+//public class SpringMVCConfig extends WebMvcConfigurationSupport {
+//
+//    //自定义mvc相关
+//    @Override
+//    public void addViewControllers(ViewControllerRegistry registry){
+//        registry.addViewController( "/" ).setViewName( "forward:login" );
+//        registry.setOrder( Ordered.HIGHEST_PRECEDENCE );
+//        super.addViewControllers( registry );
+//    }
+//}
---
+++ src/main/java/com/sm/docShare/DocShareHelper.java
@@ -0,0 +1,8 @@
+package com.sm.docShare;
+
+/**
+ * DaocShare帮助类
+ */
+public class DocShareHelper {
+    public static final String solt = "DocSahare";
+}
